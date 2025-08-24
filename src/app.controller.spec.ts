7f98822a6ac07582dc643e0cba4e82f0916e9b8f
Khant Naing Set <khantnaingset@onenex.co>
2025-08-24 20:18:03 +0700

    fix test cases

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,11 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toStrictEqual({
+        up: true,
+        ready: true,
+        message: 'App is running',
+      });
     });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,9 +17,10 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect({
+      up: true,
+      ready: true,
+      message: 'App is running',
+    });
   });
 });
