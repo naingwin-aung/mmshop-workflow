b6c92bc8ce0f2465369da264ed9a730a391c06a1
Khant Naing Set <khantnaingset@onenex.co>
2025-08-31 21:05:02 +0700

    change name

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,7 +14,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
+  describe('root controller', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toStrictEqual({
         up: true,
